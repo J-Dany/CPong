70e3759d860cb8b28811f285c452e13eb59305dd
J-Dany <jdanya@outlook.com>
2020-09-03 00:55:39 +0200

    Added some css style

--- src/main.c
+++ src/main.c
@@ -1,28 +1,57 @@
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
 #include <gtk/gtk.h>
 
-static void print_hello (GtkWidget *widget, gpointer   data)
-{
-  g_print ("Hello World\n");
-}
 
 static void on_activate (GtkApplication *app) 
 {
     GtkWidget *window;
-    GtkWidget *button;
-    GtkWidget *button_box;
+    GtkWidget *button_play, *button_quit;
+    GtkWidget *buttons_box;
 
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    GtkCssProvider *css_provider = gtk_css_provider_new();
 
-    button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add (GTK_CONTAINER (window), button_box);
+    gtk_css_provider_load_from_path(css_provider, "./style.css", NULL);
 
-    button = gtk_button_new_with_label ("Hello World");
-    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_close), window);
-    gtk_container_add (GTK_CONTAINER (button_box), button);
+    gtk_style_context_add_provider_for_screen(
+        gdk_display_get_default_screen (gdk_display_get_default()),
+        GTK_STYLE_PROVIDER(css_provider),
+        GTK_STYLE_PROVIDER_PRIORITY_THEME
+    );
 
+    window = gtk_application_window_new(app);
+    gtk_widget_set_name(GTK_WIDGET(window), "main_window");
+    gtk_window_set_title(GTK_WINDOW(window), "CPong");
+    gtk_window_set_default_size (GTK_WINDOW (window), 1280, 720);
+    GdkRGBA color = 
+    {
+        .alpha =   0,
+        .blue  = 1.0,
+        .green = 1.0,
+        .red   = 1.0
+    };
+    gtk_widget_override_background_color(GTK_WIDGET(window), GTK_WINDOW_TOPLEVEL, &color);
+
+    buttons_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
+    gtk_widget_set_name(buttons_box, "buttons_box");
+    gtk_container_add (GTK_CONTAINER (window), buttons_box);
+    gtk_widget_set_valign(buttons_box, GTK_ALIGN_CENTER);
+
+    button_play = gtk_button_new_with_label ("Play");
+    gtk_widget_set_size_request(button_play, 350, 150);
+    gtk_container_add (GTK_CONTAINER (buttons_box), button_play);
+    gtk_button_set_relief(GTK_BUTTON(button_play), GTK_RELIEF_NONE);
+    gtk_widget_set_opacity(GTK_WIDGET(button_play), 1);
+
+    button_quit = gtk_button_new_with_label("Quit");
+    gtk_widget_set_size_request(button_quit, 350, 150);
+    g_signal_connect(button_quit, "clicked", G_CALLBACK(gtk_window_close), NULL);
+    gtk_container_add(GTK_CONTAINER(buttons_box), button_quit);
+    gtk_button_set_relief(GTK_BUTTON(button_quit), GTK_RELIEF_NONE);
+
+    g_object_unref(css_provider);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     gtk_widget_show_all (window);
 }
 
