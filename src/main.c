625c618f5ce1e7caeedf8d09608cb60c4f81d169
J-Dany <jdanya@outlook.com>
2020-09-03 16:53:15 +0200

    Added background-image and change style of buttons

--- src/main.c
+++ src/main.c
@@ -10,11 +10,13 @@ static void on_activate (GtkApplication *app)
     GtkWidget *buttons_box;
 
     GtkCssProvider *css_provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default();
+    GdkScreen *screen = gdk_display_get_default_screen (display);
 
-    gtk_css_provider_load_from_path(css_provider, "./style.css", NULL);
+    gtk_css_provider_load_from_path(css_provider, "style.css", NULL);
 
     gtk_style_context_add_provider_for_screen(
-        gdk_display_get_default_screen (gdk_display_get_default()),
+        screen,
         GTK_STYLE_PROVIDER(css_provider),
         GTK_STYLE_PROVIDER_PRIORITY_USER
     );
@@ -24,18 +26,18 @@ static void on_activate (GtkApplication *app)
     gtk_window_set_title(GTK_WINDOW(window), "CPong");
     gtk_window_set_default_size (GTK_WINDOW (window), 1280, 720);
 
-    buttons_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
+    buttons_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
     gtk_widget_set_name(buttons_box, "buttons_box");
     gtk_container_add (GTK_CONTAINER (window), buttons_box);
     gtk_widget_set_valign(buttons_box, GTK_ALIGN_CENTER);
 
-    button_play = gtk_button_new_with_label ("Play");
-    gtk_widget_set_size_request(button_play, 350, 150);
+    button_play = gtk_button_new_with_label ("PLAY");
+    gtk_widget_set_size_request(button_play, 350, 135);
     gtk_container_add (GTK_CONTAINER (buttons_box), button_play);
     gtk_button_set_relief(GTK_BUTTON(button_play), GTK_RELIEF_NONE);
 
-    button_quit = gtk_button_new_with_label("Quit");
-    gtk_widget_set_size_request(button_quit, 350, 150);
+    button_quit = gtk_button_new_with_label("QUIT");
+    gtk_widget_set_size_request(button_quit, 350, 135);
     g_signal_connect(button_quit, "clicked", G_CALLBACK(gtk_window_close), NULL);
     gtk_container_add(GTK_CONTAINER(buttons_box), button_quit);
     gtk_button_set_relief(GTK_BUTTON(button_quit), GTK_RELIEF_NONE);
